0d6d0ab557cfd314ad8beaa8a15e538ed8458bdb
BuildTools <unconfigured@null.spigotmc.org>
2020-02-04 22:40:55 +0100

    Return button and plugin.yml

--- src/main/java/me/sendpacket/songmaker/gui/menu.java
+++ src/main/java/me/sendpacket/songmaker/gui/menu.java
@@ -10,24 +10,44 @@ public class menu {
 
     public static gui g = new gui("gui_1", "[Menu]");
 
-    public static void load()
+    public static void main_window()
     {
-        gui_values.gui_list.add(g);
-
         gui_window main_menu = new gui_window("main_menu",0,9,g);
 
-        main_menu.get_items().add(new gui_item("song_list","Songs","",1, Material.ORANGE_STAINED_GLASS_PANE));
-        main_menu.get_items().add(new gui_item("beat_list","Beats","",4, Material.ORANGE_STAINED_GLASS_PANE));
+        main_menu.get_items().add(new gui_item("pre_song","Songs","",1, Material.ORANGE_STAINED_GLASS_PANE));
+        main_menu.get_items().add(new gui_item("pre_beat","Beats","",4, Material.ORANGE_STAINED_GLASS_PANE));
         main_menu.get_items().add(new gui_item("song_play","Play","",7, Material.LIME_STAINED_GLASS_PANE));
         main_menu.get_items().add(new gui_item("song_stop","Stop","",8, Material.RED_STAINED_GLASS_PANE));
 
         g.set_main_window(main_menu);
+    }
 
-        gui_window pre_beat_window = new gui_window(("pre_beat"), 1, 9, g);
+    public static void pre_beat_window()
+    {
+        gui_window window = new gui_window(("pre_beat"), 2, 9, g);
 
-        pre_beat_window.get_items().add(new gui_item("list_beats", "Beats", "",1, Material.BLUE_STAINED_GLASS_PANE));
+        window.get_items().add(new gui_item("pre_beats", "Beats", "",1, Material.BLUE_STAINED_GLASS_PANE));
+        window.get_items().add(new gui_item("", "Return", "",8, 0));
 
-        g.add_window(pre_beat_window);
+        g.add_window(window);
+    }
+
+    public static void pre_song_window()
+    {
+        gui_window window = new gui_window(("pre_song"), 1, 9, g);
+
+        window.get_items().add(new gui_item("pre_songs", "Songs", "",1, Material.BLUE_STAINED_GLASS_PANE));
+        window.get_items().add(new gui_item("", "Return", "",8, 0));
+
+        g.add_window(window);
+    }
+
+    public static void load()
+    {
+        gui_values.gui_list.add(g);
+        main_window();
+        pre_beat_window();
+        pre_song_window();
     }
 
 }
--- src/main/java/me/sendpacket/songmaker/gui/menu_button.java
+++ src/main/java/me/sendpacket/songmaker/gui/menu_button.java
@@ -10,6 +10,44 @@ import org.bukkit.entity.Player;
 
 public class menu_button {
 
+    public static void update_main_window_buttons(Player p,gui_item item)
+    {
+        switch (item.get_id()) {
+            case "pre_song":
+                gui_utils.jump_to_window(p, 1);
+                break;
+            case "pre_beat":
+                gui_utils.jump_to_window(p, 2);
+                break;
+            case "song_play":
+                if(global_values.current_beat != null && global_values.current_song != null) {
+                    player.play(p, global_values.current_song.get_name(), global_values.current_beat.get_name()); // Start player
+                }
+                break;
+            case "song_stop":
+                player.stop(p); // Stop player
+                break;
+        }
+    }
+
+    public static void update_pre_song_buttons(Player p,gui_item item)
+    {
+        switch (item.get_id()) {
+            case "Songs":
+                gui_utils.jump_to_window(p, 3);
+                break;
+        }
+    }
+
+    public static void update_pre_beat_buttons(Player p,gui_item item)
+    {
+        switch (item.get_id()) {
+            case "Beats":
+                gui_utils.jump_to_window(p, 4);
+                break;
+        }
+    }
+
     public static void menu_button_update()
     {
         Bukkit.getScheduler().runTaskTimer(song_maker.get_java_plugin(), () -> {
@@ -17,29 +55,25 @@ public class menu_button {
             for(gui_window w : menu.g.get_windows())
             {
                 for(Player p : Bukkit.getOnlinePlayers()) {
-                    p.sendMessage(menu.g.get_player_list().get(p)+"");
                     for (gui_item item : w.get_items()) {
                         if (item.is_pressed(p)) {
-                            switch (item.get_id()) {
-                                case "song_list":
-                                    gui_utils.jump_to_window(p, 1);
-                                    break;
-                                case "beat_list":
-
-                                    break;
-                                case "song_play":
-                                    if(global_values.current_beat != null && global_values.current_song != null) {
-                                        player.play(p, global_values.current_song.get_name(), global_values.current_beat.get_name()); // Start player
-                                    }
-                                    break;
-                                case "song_stop":
-                                    player.stop(p); // Stop player
-                                    break;
+                            if(item.is_return_button())
+                            {
+                                gui_utils.jump_to_window(p, item.get_window_id_to_return());
+                            }else {
+                                switch (w.get_id()) {
+                                    case 0:
+                                        update_main_window_buttons(p, item);
+                                        break;
+                                    case 1:
+                                        update_pre_song_buttons(p, item);
+                                        break;
+                                    case 2:
+                                        update_pre_beat_buttons(p, item);
+                                        break;
+                                }
                             }
-                            p.sendMessage("Pressed");
                             item.not_pressed(p);
-                        }else{
-
                         }
                     }
                 }
