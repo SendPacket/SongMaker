de0a03c64bc6b7756f0636b06b598d4f00180ab3
BuildTools <unconfigured@null.spigotmc.org>
2020-02-05 01:12:18 +0100

    Menu update

--- src/main/java/me/sendpacket/songmaker/gui/menu.java
+++ src/main/java/me/sendpacket/songmaker/gui/menu.java
@@ -1,32 +1,83 @@
 package me.sendpacket.songmaker.gui;
 
-import me.sendpacket.easyguilib.gui;
-import me.sendpacket.easyguilib.gui_item;
-import me.sendpacket.easyguilib.gui_values;
-import me.sendpacket.easyguilib.gui_window;
+import me.sendpacket.easyguilib.*;
+import me.sendpacket.songmaker.global_values;
+import me.sendpacket.songmaker.player.beat.beat;
+import me.sendpacket.songmaker.player.song.song;
+import me.sendpacket.songmaker.song_maker;
 import org.bukkit.Material;
+
+import java.util.HashMap;
 
 public class menu {
 
     public static gui g = new gui("gui_1", "[Menu]");
+    public static beat tmp_beat = new beat("tmp_beat");
 
     public static void main_window()
     {
         gui_window main_menu = new gui_window("main_menu",0,9,g);
 
         main_menu.get_items().add(new gui_item("pre_song","Songs","",1, Material.ORANGE_STAINED_GLASS_PANE));
-        main_menu.get_items().add(new gui_item("pre_beat","Beats","",4, Material.ORANGE_STAINED_GLASS_PANE));
+        main_menu.get_items().add(new gui_item("pre_beat","Beats","",3, Material.ORANGE_STAINED_GLASS_PANE));
         main_menu.get_items().add(new gui_item("song_play","Play","",7, Material.LIME_STAINED_GLASS_PANE));
         main_menu.get_items().add(new gui_item("song_stop","Stop","",8, Material.RED_STAINED_GLASS_PANE));
 
         g.set_main_window(main_menu);
     }
 
+    public static void update_beat_window()
+    {
+        gui_window window = gui_utils.get_window_by_id(g, 4);
+        window.clear();
+
+        int slot = 0;
+        for(beat b : global_values.beat_list)
+        {
+            window.get_items().add(new gui_item("s_"+b.get_name(), b.get_name(), "",slot, Material.LIME_STAINED_GLASS_PANE));
+            slot += 1;
+        }
+        window.get_items().add(new gui_item("", "Return", "",53, 2));
+    }
+
+    public static void update_song_window()
+    {
+        gui_window window = gui_utils.get_window_by_id(g, 3);
+        window.clear();
+
+        int slot = 0;
+        for(song s : global_values.song_list)
+        {
+            window.get_items().add(new gui_item("s_"+s.get_name(), s.get_name(), "",slot, Material.LIME_STAINED_GLASS_PANE));
+            slot += 1;
+        }
+        window.get_items().add(new gui_item("", "Return", "",53, 2));
+    }
+
+    public static void list_beat_window()
+    {
+        gui_window window = new gui_window(("list_beat"), 4, 54, g);
+
+        g.add_window(window);
+
+        update_beat_window();
+    }
+
+    public static void list_song_window()
+    {
+        gui_window window = new gui_window(("list_song"), 3, 54, g);
+
+        g.add_window(window);
+
+        update_song_window();
+    }
+
     public static void pre_beat_window()
     {
         gui_window window = new gui_window(("pre_beat"), 2, 9, g);
 
-        window.get_items().add(new gui_item("pre_beats", "Beats", "",1, Material.BLUE_STAINED_GLASS_PANE));
+        window.get_items().add(new gui_item("beats_list", "Beats", "",1, Material.BLUE_STAINED_GLASS_PANE));
+        window.get_items().add(new gui_item("beats_create", "Create", "",3, Material.BLUE_STAINED_GLASS_PANE));
         window.get_items().add(new gui_item("", "Return", "",8, 0));
 
         g.add_window(window);
@@ -36,18 +87,68 @@ public class menu {
     {
         gui_window window = new gui_window(("pre_song"), 1, 9, g);
 
-        window.get_items().add(new gui_item("pre_songs", "Songs", "",1, Material.BLUE_STAINED_GLASS_PANE));
+        window.get_items().add(new gui_item("songs_list", "Songs", "",1, Material.BLUE_STAINED_GLASS_PANE));
         window.get_items().add(new gui_item("", "Return", "",8, 0));
 
         g.add_window(window);
     }
 
+    public static void update_create_beat_window()
+    {
+        gui_window window = gui_utils.get_window_by_id(g, 5);
+        window.clear();
+
+        for (int i = 0; i < 20; i++) {
+
+            Material mat = Material.GRAY_STAINED_GLASS_PANE;
+            switch(tmp_beat.get_sequence().get(i).get_s_num())
+            {
+                case 1:
+                    mat = Material.PURPLE_STAINED_GLASS_PANE;
+                    break;
+                case 2:
+                    mat = Material.BLUE_STAINED_GLASS_PANE;
+                    break;
+                case 3:
+                    mat = Material.CYAN_STAINED_GLASS_PANE;
+                    break;
+                case 4:
+                    mat = Material.GREEN_STAINED_GLASS_PANE;
+                    break;
+                default:
+                    break;
+            }
+
+            window.get_items().add(new gui_item("beat_tick_"+i,  tmp_beat.get_sequence().get(i).get_s_num() > 0 ? "Sound " + tmp_beat.get_sequence().get(i).get_s_num() : " ", tmp_beat.get_sequence().get(i).get_s_num() > 0 ?"Pitch " + tmp_beat.get_sequence().get(i).get_pitch() : " ", i,mat));
+        }
+
+        window.get_items().add(new gui_item("test_beat", "Test", "",23, Material.LIME_STAINED_GLASS_PANE));
+        window.get_items().add(new gui_item("save_beat", "Save", "",24, Material.ORANGE_STAINED_GLASS_PANE));
+        window.get_items().add(new gui_item("", "Return", "",26, 2));
+    }
+
+    public static void create_beat_window()
+    {
+        gui_window window = new gui_window(("create_beat"), 5, 27, g);
+
+        g.add_window(window);
+
+        update_create_beat_window();
+    }
+
     public static void load()
     {
         gui_values.gui_list.add(g);
+
         main_window();
+
         pre_beat_window();
         pre_song_window();
+
+        list_beat_window();
+        list_song_window();
+
+        create_beat_window();
     }
 
 }
--- src/main/java/me/sendpacket/songmaker/gui/menu_button.java
+++ src/main/java/me/sendpacket/songmaker/gui/menu_button.java
@@ -3,9 +3,13 @@ package me.sendpacket.songmaker.gui;
 
 import me.sendpacket.easyguilib.*;
 import me.sendpacket.songmaker.global_values;
+import me.sendpacket.songmaker.player.beat.beat;
+import me.sendpacket.songmaker.player.beat.note;
 import me.sendpacket.songmaker.player.player;
+import me.sendpacket.songmaker.player.song.song;
 import me.sendpacket.songmaker.song_maker;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 public class menu_button {
@@ -33,7 +37,8 @@ public class menu_button {
     public static void update_pre_song_buttons(Player p,gui_item item)
     {
         switch (item.get_id()) {
-            case "Songs":
+            case "songs_list":
+                menu.update_song_window();
                 gui_utils.jump_to_window(p, 3);
                 break;
         }
@@ -42,42 +47,143 @@ public class menu_button {
     public static void update_pre_beat_buttons(Player p,gui_item item)
     {
         switch (item.get_id()) {
-            case "Beats":
+            case "beats_list":
+                menu.update_beat_window();
                 gui_utils.jump_to_window(p, 4);
                 break;
+            case "beats_create":
+                menu.update_create_beat_window();
+                gui_utils.jump_to_window(p, 5);
+                break;
         }
+    }
+
+    public static void update_song_selection_buttons(Player p,int slot)
+    {
+        int i = 0;
+        for(song s : global_values.song_list)
+        {
+            if(slot == i)
+            {
+                global_values.current_song = s;
+                song_maker.get_logger_handler().send_message(p, "Selected song: " + s.get_name());
+                player.stop(p);
+            }
+            i += 1;
+        }
+    }
+
+    public static void update_beat_selection_buttons(Player p,int slot)
+    {
+        int i = 0;
+        for(beat b : global_values.beat_list)
+        {
+            if(slot == i)
+            {
+                global_values.current_beat = b;
+                song_maker.get_logger_handler().send_message(p, "Selected beat: " + b.get_name());
+                player.stop(p);
+            }
+            i += 1;
+        }
+    }
+
+    public static void update_beat_creation_buttons(Player p,int slot,gui_item item,int press_type)
+    {
+        if(slot <= 20)
+        {
+            if(press_type == 1)
+            {
+                menu.tmp_beat.get_sequence().get(slot).set_s_num(menu.tmp_beat.get_sequence().get(slot).get_s_num() > 4 ? 0 : (menu.tmp_beat.get_sequence().get(slot).get_s_num() + 1));
+            }else {
+                if (press_type == 2) {
+                    menu.tmp_beat.get_sequence().get(slot).set_pitch(menu.tmp_beat.get_sequence().get(slot).get_pitch() == 1 ? 2 : 1);
+                } else {
+                    if (press_type == 3) {
+                        menu.tmp_beat.get_sequence().get(slot).set_s_num(0);
+                        menu.tmp_beat.get_sequence().get(slot).set_pitch(1);
+                    }
+                }
+            }
+
+            switch (menu.tmp_beat.get_sequence().get(slot).get_s_num()) {
+                case 1:
+                    p.playSound(p.getLocation(), global_values.sound_1, 1.F, menu.tmp_beat.get_sequence().get(slot).get_pitch());
+                    break;
+                case 2:
+                    p.playSound(p.getLocation(), global_values.sound_2, 1.F, menu.tmp_beat.get_sequence().get(slot).get_pitch());
+                    break;
+                case 3:
+                    p.playSound(p.getLocation(), global_values.sound_3, 1.F, menu.tmp_beat.get_sequence().get(slot).get_pitch());
+                    break;
+                case 4:
+                    p.playSound(p.getLocation(), global_values.sound_4, 1.F, menu.tmp_beat.get_sequence().get(slot).get_pitch());
+                    break;
+                default:
+                    break;
+            }
+        }else{
+            switch (item.get_id()) {
+                case "test_beat":
+                    player.stop(p);
+                    global_values.current_beat_timer = 0;
+                    global_values.testing_beat = true;
+                    global_values.current_beat = menu.tmp_beat;
+                    global_values.current_beat.play();
+                    break;
+                case "save_beat":
+                    beat new_beat = new beat("beat" + global_values.beat_list.size());
+                    new_beat.set_sequence(menu.tmp_beat.get_sequence());
+                    global_values.beat_list.add(new_beat);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        menu.update_create_beat_window();
+        gui_utils.jump_to_window(p, 5);
     }
 
     public static void menu_button_update()
     {
         Bukkit.getScheduler().runTaskTimer(song_maker.get_java_plugin(), () -> {
             // GUI 1
-            for(gui_window w : menu.g.get_windows())
-            {
-                for(Player p : Bukkit.getOnlinePlayers()) {
-                    for (gui_item item : w.get_items()) {
-                        if (item.is_pressed(p)) {
-                            if(item.is_return_button())
-                            {
-                                gui_utils.jump_to_window(p, item.get_window_id_to_return());
-                            }else {
-                                switch (w.get_id()) {
-                                    case 0:
-                                        update_main_window_buttons(p, item);
-                                        break;
-                                    case 1:
-                                        update_pre_song_buttons(p, item);
-                                        break;
-                                    case 2:
-                                        update_pre_beat_buttons(p, item);
-                                        break;
+            try {
+                for (gui_window w : menu.g.get_windows()) {
+                    for (Player p : Bukkit.getOnlinePlayers()) {
+                        for (gui_item item : w.get_items()) {
+                            if (item.pressed_value(p) > 0) {
+                                if (item.is_return_button()) {
+                                    gui_utils.jump_to_window(p, item.get_window_id_to_return());
+                                } else {
+                                    switch (w.get_id()) {
+                                        case 0:
+                                            update_main_window_buttons(p, item);
+                                            break;
+                                        case 1:
+                                            update_pre_song_buttons(p, item);
+                                            break;
+                                        case 2:
+                                            update_pre_beat_buttons(p, item);
+                                            break;
+                                        case 3:
+                                            update_song_selection_buttons(p, item.get_slot());
+                                            break;
+                                        case 4:
+                                            update_beat_selection_buttons(p, item.get_slot());
+                                            break;
+                                        case 5:
+                                            update_beat_creation_buttons(p, item.get_slot(), item, item.pressed_value(p));
+                                            break;
+                                    }
                                 }
+                                item.not_pressed(p);
                             }
-                            item.not_pressed(p);
                         }
                     }
                 }
-            }
+            }catch (Exception e){};
         },0L,1L);
     }
 
